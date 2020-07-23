d80809347094db80c6d6a30a2fee93649f035169
Yohanna Mideros <mideros1@gmail.com>
2020-07-23 19:42:31 +0200

    sin errores

--- PruebasGit/src/Main.java
+++ PruebasGit/src/Main.java
@@ -21,13 +21,13 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("esto es la primera prueba");
-<<<<<<< HEAD
+//<<<<<<< HEAD
 		System.out.println("esto es la segunda prueba master"); 
 		System.out.println("esto es la segunda prueba esto es de master");
 		
-=======
+//=======
 		System.out.println("esto es una prueba en proyecto de yohanna"); 
->>>>>>> devYohanna
+//>>>>>>> devYohanna
 	}
 
 }
