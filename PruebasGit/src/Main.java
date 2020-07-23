baf7fc4916654982801260df66a2e68591d49bac
Yohanna Mideros <mideros1@gmail.com>
2020-07-23 19:20:28 +0200

    este es primer commit desde devyohanna

--- PruebasGit/src/Main.java
+++ PruebasGit/src/Main.java
@@ -21,7 +21,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("esto es la primera prueba");
-		System.out.println("esto es la segunda prueba");
+		System.out.println("esto es una prueba en proyecto de yohanna"); 
 	}
 
 }
