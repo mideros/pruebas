d82547a89927bc71840d9e1d9b7ac553cee6abaf
Yohanna Mideros <mideros1@gmail.com>
2020-07-23 18:33:12 +0200

    segunda prueba

--- PruebasGit/src/Main.java
+++ PruebasGit/src/Main.java
@@ -21,6 +21,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("esto es la primera prueba");
+		System.out.println("esto es la segunda prueba");
 	}
 
 }
