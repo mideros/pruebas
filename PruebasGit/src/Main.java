d10a93ff515ee24b2e6961c124be74023c545ee9
Yohanna Mideros <mideros1@gmail.com>
2020-07-23 19:25:51 +0200

    este commit es de master

--- PruebasGit/src/Main.java
+++ PruebasGit/src/Main.java
@@ -21,7 +21,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("esto es la primera prueba");
-		System.out.println("esto es la segunda prueba");
+		System.out.println("esto es la segunda prueba master"); 
+		System.out.println("esto es la segunda prueba esto es de master");
+		
 	}
 
 }
